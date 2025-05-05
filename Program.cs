3b12e6be4292b6f3e85bf7514e4d70d49baeaf8b
Beatriz Batista <byah24@outlook.com>
2025-05-05 10:44:39 -0300

    Programa Finalizado 80%

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear(); 
+
+int numero = 0;
+
+Console.WriteLine("Digite um número:");
+bool numeroEhPar = int.TryParse(Console.ReadLine(), out numero);
+//numero = Convert.ToInt32(Console.ReadLine()!);
+
+if (numeroEhPar && numero % 2 == 0)
+{
+    Console.WriteLine($"O número {numero} é par");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ímpar");
+}
+
+// if (numero % 2 == 0)
+// {
+//     Console.WriteLine($"O número {numero} é par");
+// }
+
+// else 
+// {
+//     Console.WriteLine($"O número {numero} é ímpar");
+// }
+
+
